51f315535315fb32a2a2787b8526f8eaaff75a50
Joseph Johnston <1193347519@qq.com>
2022-04-20 23:48:49 +0800

    343, 初版

--- src/编程随想录/动态规划/f1基础题目/343integerBreak.java
+++ src/编程随想录/动态规划/f1基础题目/343integerBreak.java
@@ -2,8 +2,26 @@ package 编程随想录.动态规划.f1基础题目;
 
 class Solution343 {
     // 重叠子问题，切成两个数的最大乘积必定是这两个数切分的最大乘积的乘积
+    // 二维实现，自己的解法
     public int integerBreak(int n) {
+        int[] memory = new int[n + 1];
+        memory[1] = 1;
 
-        return n;
+        for (int i = 2; i <= n; i++) {
+            int roundMax = 1;
+            // 优化：j = i / 2
+            for (int j = i / 2; j < i; j++) {
+                int tmp = i - j;
+                int tmpMax;
+
+                tmpMax = Math.max(tmp * memory[j], memory[tmp] * j);
+                tmpMax = Math.max(tmpMax, tmp * j);
+
+                roundMax = Math.max(roundMax, tmpMax);
+            }
+            memory[i] = roundMax;
+        }
+
+        return memory[n];
     }
 }
