6f2e77aece0502b65aa496710a6a5d6b16d2822e
Joseph Johnston <1193347519@qq.com>
2022-04-20 23:55:16 +0800

    343, 贪心算法

--- src/编程随想录/动态规划/f1基础题目/343integerBreak.java
+++ src/编程随想录/动态规划/f1基础题目/343integerBreak.java
@@ -1,27 +1,26 @@
 package 编程随想录.动态规划.f1基础题目;
 
+/**
+ * https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0343.%E6%95%B4%E6%95%B0%E6%8B%86%E5%88%86.md
+ */
 class Solution343 {
+
     // 重叠子问题，切成两个数的最大乘积必定是这两个数切分的最大乘积的乘积
-    // 二维实现，自己的解法
     public int integerBreak(int n) {
         int[] memory = new int[n + 1];
         memory[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-            int roundMax = 1;
-            // 优化：j = i / 2
-            for (int j = i / 2; j < i; j++) {
-                int tmp = i - j;
-                int tmpMax;
-
-                tmpMax = Math.max(tmp * memory[j], memory[tmp] * j);
-                tmpMax = Math.max(tmpMax, tmp * j);
-
-                roundMax = Math.max(roundMax, tmpMax);
+            for (int j = 1; j < i; j++) {
+                // 代码最少的写法，题解实现
+                memory[i] = Math.max(memory[i],
+                        Math.max((i - j) * j, memory[i - j] * j));
             }
-            memory[i] = roundMax;
         }
 
         return memory[n];
     }
+
+    // 还有一个贪心算法，但需要数学证明
+    // 每次拆成 n 个 3，如果剩下是 4，则保留 4，然后相乘
 }
