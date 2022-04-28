733731e3d35fa57a05450143c7475b6698a25af1
Joseph Johnston <1193347519@qq.com>
2022-04-28 23:57:26 +0800

    416, 研究 case

--- src/编程随想录/动态规划/f2背包问题/416canPartition.java
+++ src/编程随想录/动态规划/f2背包问题/416canPartition.java
@@ -2,6 +2,7 @@ package 编程随想录.动态规划.f2背包问题;
 
 class Solution416 {
     // 如何暴力破解？回溯当然可以
+    // 0-1 背包变体，依旧很难
     public boolean canPartition(int[] nums) {
         int sum = 0;
         for (int num : nums) {
@@ -20,17 +21,55 @@ class Solution416 {
             dp[0][i] = nums[0];
         }
 
+        // i 是数组，j 是容量
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j <= target; j++) {
                 // 背包容量可以容纳 nums[i]
                 if (j >= nums[i]) {
+                    /*
+                    对于在容量 j，对第 i 个数字进行判断时：
+                        或者选择第 i-1 个数字时的决定
+                        或者选择花费容量 nums[i] 来装载容量 nums[i]
+                     */
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - nums[i]] + nums[i]);
+                    /*
+                    没有 Math.max：
+                        case：[2, 2, 1, 1]
+                     */
+//                    dp[i][j] = dp[i - 1][j - nums[i]] + nums[i];
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
+        for (int[] ints : dp) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+
         return dp[nums.length - 1][target] == target;
+    }
+
+    public static void main(String[] args) {
+        Solution416 solution416 = new Solution416();
+
+        /*
+        wrong:
+        0 0 2 2
+        0 0 2 2
+        0 1 1 3
+        0 1 2 2
+
+        right:
+        0 0 2 2
+        0 0 2 2
+        0 1 2 3
+        0 1 2 3
+         */
+        int[] nums = new int[]{ 2, 2, 1, 1 };
+        solution416.canPartition(nums);
     }
 }
