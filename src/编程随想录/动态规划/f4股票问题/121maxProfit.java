c39cadf460741df0ad41325ec3d2db9d764b2b4c
Joseph Johnston <1193347519@qq.com>
2022-05-18 22:09:34 +0800

    121，抄答案，理解

---
+++ src/编程随想录/动态规划/f4股票问题/121maxProfit.java
@@ -0,0 +1,24 @@
+package 编程随想录.动态规划.f4股票问题;
+
+class Solution121 {
+    // 显然暴力法可以解
+    // 动态规划是二维数组
+    public int maxProfit(int[] prices) {
+        int length = prices.length;
+
+        if (length == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[length][2];
+        dp[0][0] -= prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+        }
+
+        return dp[length - 1][1];
+    }
+}
