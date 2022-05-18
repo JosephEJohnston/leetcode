b1c42399ad63ec3ace85d6ba278f768002406b02
Joseph Johnston <1193347519@qq.com>
2022-05-18 22:15:37 +0800

    121，注释

--- src/编程随想录/动态规划/f4股票问题/121maxProfit.java
+++ src/编程随想录/动态规划/f4股票问题/121maxProfit.java
@@ -15,7 +15,9 @@ class Solution121 {
         dp[0][1] = 0;
 
         for (int i = 1; i < length; i++) {
+            // 第一维存最低的成本
             dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            // 第二维存最高利润
             dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
         }
 
