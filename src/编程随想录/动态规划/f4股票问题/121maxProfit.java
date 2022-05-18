8e4918a471cdc087b289686b87b4ed715333e580
Joseph Johnston <1193347519@qq.com>
2022-05-18 22:28:00 +0800

    121，动归最优解

--- src/编程随想录/动态规划/f4股票问题/121maxProfit.java
+++ src/编程随想录/动态规划/f4股票问题/121maxProfit.java
@@ -10,17 +10,17 @@ class Solution121 {
             return 0;
         }
 
-        int[][] dp = new int[length][2];
+        int[][] dp = new int[2][2];
         dp[0][0] -= prices[0];
         dp[0][1] = 0;
 
         for (int i = 1; i < length; i++) {
             // 第一维存最低的成本
-            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], -prices[i]);
             // 第二维存最高利润
-            dp[i][1] = Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0]);
+            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], prices[i] + dp[(i - 1) % 2][0]);
         }
 
-        return dp[length - 1][1];
+        return dp[(length - 1) % 2][1];
     }
 }
