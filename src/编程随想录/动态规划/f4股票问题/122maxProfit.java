49a7035a9adf2c3f903406db3c7be55163b928a6
Joseph Johnston <1193347519@qq.com>
2022-05-19 23:06:35 +0800

    122，抄答案，思考

---
+++ src/编程随想录/动态规划/f4股票问题/122maxProfit.java
@@ -0,0 +1,32 @@
+package 编程随想录.动态规划.f4股票问题;
+
+class Solution122 {
+    // 看不出这和动态规划有啥关系
+    /**
+     * dp[i][0] 表示第 i 天持有股票所得现金
+     * dp[i][1] 表示第 i 天不持有股票所得最多现金
+     */
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < n; i++) {
+            // 持有
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            // 不持有
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        return dp[n - 1][1];
+    }
+
+    public static void main(String[] args) {
+        Solution122 solution122 = new Solution122();
+
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        solution122.maxProfit(prices);
+    }
+}
