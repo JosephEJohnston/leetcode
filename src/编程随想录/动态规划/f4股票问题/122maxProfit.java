ec67ddb56db91a5bd46665f635a8a58ef8fc4e39
Joseph Johnston <1193347519@qq.com>
2022-05-19 23:16:34 +0800

    122，最优解

--- src/编程随想录/动态规划/f4股票问题/122maxProfit.java
+++ src/编程随想录/动态规划/f4股票问题/122maxProfit.java
@@ -8,19 +8,19 @@ class Solution122 {
      */
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
+        int[][] dp = new int[2][2];
 
         dp[0][0] = -prices[0];
         dp[0][1] = 0;
 
         for (int i = 1; i < n; i++) {
             // 持有
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] - prices[i]);
             // 不持有
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0] + prices[i]);
         }
 
-        return dp[n - 1][1];
+        return dp[(n - 1) % 2][1];
     }
 
     public static void main(String[] args) {
