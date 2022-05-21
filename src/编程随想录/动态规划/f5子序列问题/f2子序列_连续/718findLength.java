75edf30358d4c7798ff89c5c6b47f163013fdb00
Joseph Johnston <1193347519@qq.com>
2022-05-22 06:10:17 +0800

    718, 最优解

--- src/编程随想录/动态规划/f5子序列问题/f2子序列_连续/718findLength.java
+++ src/编程随想录/动态规划/f5子序列问题/f2子序列_连续/718findLength.java
@@ -10,21 +10,32 @@ class Solution718 {
      * dp[i][j] 的状态只能由 dp[i - 1][j - 1] 推到出来
      */
     public int findLength(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int[] dp = new int[nums2.length + 1];
 
         int result = 0;
         for (int i = 1; i <= nums1.length; i++) {
-            for (int j = 1; j <= nums2.length; j++) {
+            // 倒序是为了避免覆盖，有点怪
+            for (int j = nums2.length; j > 0; j--) {
                 if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[j] = dp[j - 1] + 1;
+                } else {
+                    dp[j] = 0;
                 }
 
-                if (dp[i][j] > result) {
-                    result = dp[i][j];
+                if (dp[j] > result) {
+                    result = dp[j];
                 }
+
             }
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        Solution718 solution718 = new Solution718();
+
+        solution718.findLength(new int[]{1, 0, 0, 0, 1},
+                new int[]{1, 0, 0, 1, 1});
     }
 }
