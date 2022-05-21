3365ffd116582513ecb01d45b0fd86136a5c0204
Joseph Johnston <1193347519@qq.com>
2022-05-21 23:14:33 +0800

    1035，看答案

---
+++ src/编程随想录/动态规划/f5子序列问题/f1子序列_不连续/1035maxUncrossedLines.java
@@ -0,0 +1,23 @@
+package 编程随想录.动态规划.f5子序列问题.f1子序列_不连续;
+
+class Solution1035 {
+    /**
+     * 和 1143 很像，但没思路，看答案吧
+     * 然而其实就是同一道题
+     */
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[nums1.length][nums2.length];
+    }
+}
