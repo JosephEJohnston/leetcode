fe1054d5badda497c2b89dc551013a74195bbd07
Joseph Johnston <1193347519@qq.com>
2022-05-21 17:48:52 +0800

    123，最优解

--- src/编程随想录/动态规划/f4股票问题/123maxProfit.java
+++ src/编程随想录/动态规划/f4股票问题/123maxProfit.java
@@ -18,27 +18,19 @@ class Solution123 {
             return 0;
         }
 
-        int[][] dp = new int[prices.length][5];
+        int[] dp = new int[5];
 
-        dp[0][1] = -prices[0];
-        dp[0][3] = -prices[0];
+        dp[1] = -prices[0];
+        dp[3] = -prices[0];
 
         for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = dp[i - 1][0];
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
-            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
-            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
-            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+            dp[1] = Math.max(dp[1], dp[0] - prices[i]);
+            dp[2] = Math.max(dp[2], dp[1] + prices[i]);
+            dp[3] = Math.max(dp[3], dp[2] - prices[i]);
+            dp[4] = Math.max(dp[4], dp[3] + prices[i]);
         }
 
-        /*for (int i = 0; i < 5; i++) {
-            for (int[] ints : dp) {
-                System.out.printf("%5d ", ints[i]);
-            }
-            System.out.println();
-        }*/
-
-        return dp[prices.length - 1][4];
+        return dp[4];
     }
 
     public static void main(String[] args) {
