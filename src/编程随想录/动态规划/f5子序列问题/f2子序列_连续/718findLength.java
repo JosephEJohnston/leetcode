7329282b2caf0e4c27923540a263bff964204076
Joseph Johnston <1193347519@qq.com>
2022-05-22 05:56:17 +0800

    718, 注释

--- src/编程随想录/动态规划/f5子序列问题/f2子序列_连续/718findLength.java
+++ src/编程随想录/动态规划/f5子序列问题/f2子序列_连续/718findLength.java
@@ -2,6 +2,13 @@ package 编程随想录.动态规划.f5子序列问题.f2子序列_连续;
 
 class Solution718 {
     // 刷答案吧
+
+    /**
+     * dp[i][j]：以下标为 i-1 为结尾的 A，和以下标 j-1 为结尾的 B
+     *      最长重复子数组长度为 dp[i][j]
+     *
+     * dp[i][j] 的状态只能由 dp[i - 1][j - 1] 推到出来
+     */
     public int findLength(int[] nums1, int[] nums2) {
         int[][] dp = new int[nums1.length + 1][nums2.length + 1];
 
