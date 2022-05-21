ce39f5785554cd88a734d91665433c79749b462b
Joseph Johnston <1193347519@qq.com>
2022-05-22 07:28:17 +0800

    392，优化

--- src/编程随想录/动态规划/f5子序列问题/f3编辑距离/392isSubsequence.java
+++ src/编程随想录/动态规划/f5子序列问题/f3编辑距离/392isSubsequence.java
@@ -12,7 +12,7 @@ class Solution392 {
                 if (sChars[i - 1] == tChars[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] =  dp[i][j - 1];
                 }
             }
         }
