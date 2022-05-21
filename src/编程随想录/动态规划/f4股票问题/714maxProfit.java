8a4b1f4f3f3503fd81723136d3820896082be4c2
Joseph Johnston <1193347519@qq.com>
2022-05-21 21:18:54 +0800

    714，再优化

--- src/编程随想录/动态规划/f4股票问题/714maxProfit.java
+++ src/编程随想录/动态规划/f4股票问题/714maxProfit.java
@@ -8,14 +8,14 @@ class Solution714 {
         }
 
         // 0：买入，1：卖出
-        int[][] dp = new int[2][2];
-        dp[0][0] = -prices[0] - fee;
+        int[] dp = new int[2];
+        dp[0] = -prices[0] - fee;
 
         for (int i = 1; i < n; i++) {
-            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] - prices[i] - fee);
-            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0] + prices[i]);
+            dp[0] = Math.max(dp[0], dp[1] - prices[i] - fee);
+            dp[1] = Math.max(dp[1], dp[0] + prices[i]);
         }
 
-        return dp[(n - 1) % 2][1];
+        return dp[1];
     }
 }
