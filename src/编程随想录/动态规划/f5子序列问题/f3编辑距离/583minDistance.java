37004ccb49de3f59ea37a0a9975db45cb56f7b51
Joseph Johnston <1193347519@qq.com>
2022-05-22 15:45:08 +0800

    583，自解实现

---
+++ src/编程随想录/动态规划/f5子序列问题/f3编辑距离/583minDistance.java
@@ -0,0 +1,32 @@
+package 编程随想录.动态规划.f5子序列问题.f3编辑距离;
+
+class Solution583 {
+    /**
+     * 自己手写所有结果，列成表格，总结规律
+     */
+    public int minDistance(String word1, String word2) {
+        char[] chars1 = word1.toCharArray();
+        char[] chars2 = word2.toCharArray();
+
+        int[][] dp = new int[chars1.length + 1][chars2.length + 1];
+        for (int i = 0; i <= chars1.length; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 0; i <= chars2.length ; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= chars1.length; i++) {
+            for (int j = 1; j <= chars2.length; j++) {
+                if (chars1[i - 1] == chars2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        return dp[chars1.length][chars2.length];
+    }
+}
