1c3bb27d600b4f1b4167793fe114967794858fd6
Joseph Johnston <1193347519@qq.com>
2022-05-22 21:06:36 +0800

    516，改正

--- src/编程随想录/动态规划/f5子序列问题/f4回文/516longestPalindromeSubseq.java
+++ src/编程随想录/动态规划/f5子序列问题/f4回文/516longestPalindromeSubseq.java
@@ -11,7 +11,12 @@ class Solution516 {
 
         int[][] dp = new int[n][n];
 
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
         for (int i = n - 1; i >= 0; i--) {
+            // i, j 不能相同
             for (int j = i + 1; j < n; j++) {
                 if (chars[i] == chars[j]) {
                     dp[i][j] = dp[i + 1][j - 1] + 2;
