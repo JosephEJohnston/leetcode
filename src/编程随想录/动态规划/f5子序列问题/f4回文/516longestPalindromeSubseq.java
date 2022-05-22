4a88c48af339f644ae9246a0d6497423334df574
Joseph Johnston <1193347519@qq.com>
2022-05-22 20:59:47 +0800

    516，抄答案

---
+++ src/编程随想录/动态规划/f5子序列问题/f4回文/516longestPalindromeSubseq.java
@@ -0,0 +1,26 @@
+package 编程随想录.动态规划.f5子序列问题.f4回文;
+
+class Solution516 {
+    /**
+     * 看答案吧
+     *
+     */
+    public int longestPalindromeSubseq(String s) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (chars[i] == chars[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+}
