2974ea8b87d8f13d281ed04770a43381fe58a650
Joseph Johnston <1193347519@qq.com>
2022-05-28 17:44:52 +0800

    704，二分查找，做了多次

---
+++ src/编程随想录/f0数组/704search.java
@@ -0,0 +1,21 @@
+package 编程随想录.f0数组;
+
+class Solution704 {
+    // 其实不太简单
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
