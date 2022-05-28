9506c3870e6ca1b436a37f07b64f93fa1446aa5d
Joseph Johnston <1193347519@qq.com>
2022-05-28 20:28:26 +0800

    209，抄答案

---
+++ src/编程随想录/f0数组/209minSubArrayLen.java
@@ -0,0 +1,24 @@
+package 编程随想录.f0数组;
+
+class Solution209 {
+    // 看答案
+    // 滑动窗口
+    public int minSubArrayLen(int target, int[] nums) {
+        int result = Integer.MAX_VALUE;
+        int sum = 0;
+
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+
+            // 滑动窗口的精妙之处在于，根据当前子序列和大小的情况，不断调节子序列的起始位置
+            while (sum >= target) {
+                int subLength = (j - i + 1);
+                result = Math.min(result, subLength);
+                sum -= nums[i++];
+            }
+        }
+
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+}
