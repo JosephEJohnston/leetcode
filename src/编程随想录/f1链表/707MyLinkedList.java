c3fb6a45d14a6194cf48d27d8645a6df078c6356
Joseph Johnston <1193347519@qq.com>
2022-05-28 23:23:19 +0800

    707, init

---
+++ src/编程随想录/f1链表/707MyLinkedList.java
@@ -0,0 +1,39 @@
+package 编程随想录.f1链表;
+
+class MyLinkedList {
+
+    public MyLinkedList() {
+
+    }
+
+    public int get(int index) {
+
+        return 0;
+    }
+
+    public void addAtHead(int val) {
+
+    }
+
+    public void addAtTail(int val) {
+
+    }
+
+    public void addAtIndex(int index, int val) {
+
+    }
+
+    public void deleteAtIndex(int index) {
+
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
