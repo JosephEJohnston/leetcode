6841f36087af863110af938db195617752c32a70
Joseph Johnston <1193347519@qq.com>
2022-06-03 14:13:14 +0800

    offer58, 自解实现

--- src/编程随想录/f3字符串/Offer58reverseLeftWords.java
+++ src/编程随想录/f3字符串/Offer58reverseLeftWords.java
@@ -2,8 +2,20 @@ package 编程随想录.f3字符串;
 
 class SolutionOffer58 {
     public String reverseLeftWords(String s, int n) {
+        char[] result = new char[s.length()];
 
+        int border = n - 1;
+        int right = result.length - 1;
+        while (border >= 0) {
+            result[right] = s.charAt(border);
+            right--;
+            border--;
+        }
 
-        return "";
+        for (int i = n, j = 0; j <= right; i++, j++) {
+            result[j] = s.charAt(i);
+        }
+
+        return new String(result);
     }
 }
