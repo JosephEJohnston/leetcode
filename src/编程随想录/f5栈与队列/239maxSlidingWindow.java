0a3ffdfba24390c82caaee995081d842944b98c6
Joseph Johnston <1193347519@qq.com>
2022-06-09 23:18:10 +0800

    239, 抄答案实现，双向队列

--- src/编程随想录/f5栈与队列/239maxSlidingWindow.java
+++ src/编程随想录/f5栈与队列/239maxSlidingWindow.java
@@ -7,44 +7,41 @@ class Solution239 {
     // 应该是单调栈
     // 实际上双向队列很好解
     // 看答案了，就是单调队列
+    // 还有一个索引的问题
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] res = new int[nums.length - k + 1];
-
         Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < k; i++) {
-            if (!deque.isEmpty()) {
-                while (!deque.isEmpty() && nums[i] > deque.peek()) {
-                    deque.pop();
-                }
+
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            // 队列头节点需要在 [i-k+1, i] 范围内，否则要弹出
+            while (!deque.isEmpty() && deque.getLast() < i - k + 1) {
+                deque.removeLast();
             }
 
-            deque.push(nums[i]);
-        }
-        res[0] = deque.getLast();
-
-        for (int i = k, j = 1; i < nums.length; i++, j++) {
-            deque.removeLast();
-
-            if (!deque.isEmpty()) {
-                while (!deque.isEmpty() && nums[i] > deque.peek()) {
-                    deque.pop();
-                }
+            // 既然是单调，就要保证每次放进去的数据要比末尾的都大，否则也弹出
+            while (!deque.isEmpty() && nums[deque.peek()] < nums[i]) {
+                deque.pop();
             }
 
-            deque.push(nums[i]);
-            res[j] = deque.getLast();
-        }
+            deque.push(i);
 
+            if (i >= k - 1) {
+                res[idx++] = nums[deque.getLast()];
+            }
+        }
 
         return res;
     }
 
     public static void main(String[] args) {
 //        int[] nums = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
-        int[] nums = new int[]{7, 2, 4};
+        int[] nums = new int[]{1,3,-1,-3,5,3,6,7};
 
         Solution239 solution239 = new Solution239();
-        solution239.maxSlidingWindow(nums, 2);
+        solution239.maxSlidingWindow(nums, 3);
 
         /*Deque<Integer> deque = new LinkedList<>();
 
