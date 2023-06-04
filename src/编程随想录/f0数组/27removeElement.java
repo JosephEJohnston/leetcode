fc3b74f8118ea338612a49c2321c507e30627245
JosephEJohnston <1193347519@qq.com>
2023-06-04 19:24:14 +0800

    27, 二刷

--- src/编程随想录/f0数组/27removeElement.java
+++ src/编程随想录/f0数组/27removeElement.java
@@ -2,20 +2,21 @@ package 编程随想录.f0数组;
 
 class Solution27 {
     // 没什么难的
+    // 2023/6/4 二刷
     public int removeElement(int[] nums, int val) {
-        int curIndex = 0;
-        int length = 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
 
+        int curIndex = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == val) {
                 continue;
             }
 
-            nums[curIndex] = nums[i];
-            curIndex++;
-            length++;
+            nums[curIndex++] = nums[i];
         }
 
-        return length;
+        return curIndex;
     }
 }
